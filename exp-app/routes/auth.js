f0437b3d59ac7b707310203362961e823bf20b2f
Sonit <sonitbuilds+git@gmail.com>
2023-05-13 08:51:46 -0700

    passport

--- exp-app/routes/auth.js
+++ exp-app/routes/auth.js
@@ -2,15 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/forgot-password", function (req, res) {
-  res.render("auth/forgot-password/index", { title: "Forgot Password" });
+  res.render("auth/forgot-password/index", { title: "Aarya: Forgot Password" });
 });
 
 router.get("/login", function (req, res) {
-  res.render("auth/login/index", { title: "Login" });
+  res.render("auth/login/index", { title: "Aarya: Login" });
 });
 
 router.get("/signup", async function (req, res) {
-  res.render("auth/signup/index", { title: "Signup" });
+  res.render("auth/signup/index", { title: "Aarya: Signup" });
 });
 
 module.exports = router;
