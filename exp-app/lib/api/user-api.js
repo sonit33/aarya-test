90dbbf8e356aba697748698868cb21a25e0482e5
Sonit <sonitbuilds+git@gmail.com>
2023-05-16 00:10:27 -0700

    reset password tests

--- exp-app/lib/api/user-api.js
+++ exp-app/lib/api/user-api.js
@@ -12,16 +12,19 @@ module.exports = class User {
     return randomInt(100000, 999999).toString();
   }
 
-  async makeHash(password, salt) {
+  async makeHashAsync(password, salt) {
+    if (!password) throw new Error("empty password");
+    if (!salt) throw new Error("empty salt");
     return (await pbkdf2Async(password, salt, 310000, 32, "sha256")).toString("hex");
   }
 
   verifyHash(source, target) {
+    if (!source) throw new Error("empty password");
+    if (!target) throw new Error("empty salt");
     return timingSafeEqual(Buffer.from(source, "utf-8"), Buffer.from(target, "utf-8"));
   }
 
   async insert(firstName, lastName, email, passwordHash, salt) {
-    console.log(arguments);
     const user = new model({
       provider: "email",
       email: email,
@@ -53,12 +56,12 @@ module.exports = class User {
   async create(firstName, lastName, email, password) {
     try {
       const salt = this.makeSalt();
-      const passwordHash = await this.makeHash(password, salt);
+      const passwordHash = await this.makeHashAsync(password, salt);
       const userId = await this.insert(firstName, lastName, email, passwordHash, salt);
       return userId;
     } catch (e) {
-      console.log(e);
-      throw new Error("Create User Failed", e);
+      console.log(e.message);
+      throw new Error("Create User Failed", e.message);
     }
   }
 
@@ -96,12 +99,39 @@ module.exports = class User {
 
   async verifyEmail(userId, code) {
     const user = await this.findById(userId);
-    if (user.code == code) {
-      user.updateOne({ isVerified: true });
-    } else {
-      const msg = `Email verification failed for ${userId} with code ${code}`;
-      console.log(msg);
-      throw new Error(msg);
+    if (!user.code == code)
+      throw new Error(`Email verification failed for ${userId} with code ${code}`);
+    await user.updateOne({ isVerified: true });
+  }
+
+  async generateNewCode(userId) {
+    const user = await this.findById(userId);
+    const code = this.makeSixDigitCode();
+    user.updateOne({ verificationCode: code });
+    return code;
+  }
+
+  async changePassword(userId, oldPassword, newPassword) {
+    const user = await this.findById(userId);
+    const hash = await this.makeHashAsync(oldPassword, user.salt);
+    if (!this.verifyHash(hash, user.passwordHash))
+      throw new Error(`Old password is incorrect for user: ${userId}`);
+    await user.updateOne({ passwordHash: this.makeHashAsync(newPassword, user.salt) });
+  }
+
+  async resetPassword(userId, code, newPassword) {
+    const user = await this.findById(userId);
+    if (!user.verificationCode == code)
+      throw new Error(`Invalid verification code for user: ${userId}`);
+    await user.updateOne({ passwordHash: await this.makeHashAsync(newPassword, user.salt) });
+  }
+
+  async matchPassword(password, passwordHash, salt) {
+    try {
+      const cookedHash = await this.makeHashAsync(password, salt);
+      return this.verifyHash(cookedHash, passwordHash);
+    } catch (e) {
+      throw new Error(`Failed to match passwords ${e.message}`);
     }
   }
 };
--- exp-app/tests/auth.test.js
+++ exp-app/tests/auth.test.js
@@ -13,7 +13,7 @@ afterAll(async function () {
 });
 
 test("cooks a hash", async function () {
-  expect(await api.makeHash("abcd", "1234")).toBe(
+  expect(await api.makeHashAsync("abcd", "1234")).toBe(
     "be18802f60ee2022dab13d37987babcccb257a2a0bec8284295a7e3ee4e77cbe"
   );
 });
@@ -27,7 +27,7 @@ test("makes random six-digit code", function () {
 });
 
 test("verifies hash", async function () {
-  const cookedHash = await api.makeHash("abcd", "1234");
+  const cookedHash = await api.makeHashAsync("abcd", "1234");
   expect(
     api.verifyHash(cookedHash, "be18802f60ee2022dab13d37987babcccb257a2a0bec8284295a7e3ee4e77cbe")
   ).toBe(true);
@@ -58,4 +58,47 @@ test("create a new user with provider", async function () {
   expect("sss@xyz.com").toBe(user.email);
   expect(user.passwordHash).not.toBe(null);
   expect(user.isVerified).toBe(true);
+});
+
+test("duplicate email fails to signup", async function () {
+  const email = "abc@xyz.com";
+  const password = "12345abc";
+  const x = () => api.create("x", "y", email, password);
+  expect(x).rejects.toThrow(Error);
+});
+
+test("user logs in", async function () {
+  const email = "abc@xyz.com";
+  const password = "12345abc";
+  let user = await api.findByEmail(email);
+  const hash = await api.makeHashAsync(password, user.salt);
+  expect(api.verifyHash(hash, user.passwordHash)).toBe(true);
+});
+
+test("user fails to log in", async function () {
+  const email = "abc@xyz.com";
+  const password = "wrong-password";
+  let user = await api.findByEmail(email);
+  const hash = await api.makeHashAsync(password, user.salt);
+  expect(api.verifyHash(hash, user.passwordHash)).toBe(false);
+});
+
+test("user with provider fails to log in with password", async function () {
+  const email = "sss@xyz.com";
+  const password = "some-password";
+  let user = await api.findByEmail(email);
+  expect(user.salt).toBe(undefined);
+  expect(user.passwordHash).toBe(undefined);
+  const t = () => api.makeHashAsync(password, user.salt);
+  expect(t).rejects.toThrow(Error);
+  const x = () => api.verifyHash(undefined, user.passwordHash);
+  expect(x).toThrow(Error);
+});
+
+test("user with no password can't reset password (e.g. google user using forgot password)", async function () {
+  const email = "sss@xyz.com";
+  let user = await api.findByEmail(email);
+  let id = user._id.toString();
+  let code = user.verificationCode;
+  await expect(api.resetPassword(id, code, "new-password")).rejects.toThrow(Error);
 });
